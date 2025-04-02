1335915a6157746b0cad2598dfdec4b134b8b488
xuyil <xuyil@yealink.com>
2025-04-02 15:36:40 +0800

    fix

--- packages/server/lib/router.js
+++ packages/server/lib/router.js
@@ -1,14 +1,14 @@
-const Router = require("koa-router");
-let router = new Router();
+const Router = require('koa-router')
+let router = new Router()
 
-router.get("/", ctx => {
-    ctx.body = "some value..";
+router.get('/', (ctx) => {
+  ctx.body = 'some value..'
 })
-router.get("/getList", ctx => {
-    ctx.body = 'aaa';
+router.get('/a', (ctx) => {
+  ctx.body = 'aaa'
 })
-router.get("/getOvertimeHours", ctx => {
-    ctx.body = {data:3};
+router.get('/aa', (ctx) => {
+  ctx.body = { data: 3 }
 })
 
 module.exports = router
